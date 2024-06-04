21dde1e2dfaed8c7a525465b83e3ce1db5ea76b3
FavorKim <harangs99@daum.net>
2024-06-04 13:57:31 +0900

    [init] 게임 맵 에셋 추가 인게임 플레이어 이동 구현중

--- Assets/Scripts/Player/GamePlayer.cs
+++ Assets/Scripts/Player/GamePlayer.cs
@@ -2,8 +2,43 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GamePlayer : NetworkBehaviour
 {
-    
+    CharacterController _controller;
+    [SerializeField] GameObject PlayerHead;
+
+    Vector3 _moveDir;
+
+    [SyncVar, SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
+
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        _controller.SimpleMove(_moveDir);
+    }
+
+    public void OnMove(InputValue val)
+    {
+        Vector2 dir = val.Get<Vector2>();
+        _moveDir = new Vector3(dir.x, 0, dir.y) * _moveSpeed * Time.deltaTime;
+    }
+
+    public void OnRotate(InputValue val)
+    {
+        Vector2 delta = val.Get<Vector2>();
+        PlayerHead.transform.Rotate(delta * _rotateSpeed * Time.deltaTime);
+    }
 }
--- Assets/Scripts/Player/RoomPlayer.cs
+++ Assets/Scripts/Player/RoomPlayer.cs
@@ -10,9 +10,15 @@ public class RoomPlayer : NetworkRoomPlayer
     [SerializeField] private string _id;
     [SerializeField] private TMP_InputField _maxConnection;
 
-    [SyncVar] private float _playerSpeed = 2f;
+    [SyncVar, SerializeField] private float _playerSpeed = 2f;
+    Animator _anim;
 
     private Vector2 _playerMoveDir;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
 
     public override void OnStartClient()
     {
@@ -38,8 +44,13 @@ public class RoomPlayer : NetworkRoomPlayer
         if (moveDir != Vector2.zero)
         {
             _playerMoveDir = moveDir * _playerSpeed * Time.deltaTime;
+            _anim.SetBool("isWalk", true);
         }
         else
+        {
             _playerMoveDir = Vector2.zero;
+            _anim.SetBool("isWalk", false);
+        }
     }
+
 }
