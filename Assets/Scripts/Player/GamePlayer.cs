157e68e2c6e53642dfcc86d13354a827abf854a5
FavorKim <harangs99@daum.net>
2024-06-04 15:16:33 +0900

    [feat] RoomUI.cs 및 게임 신 시작 지점 추가

--- Assets/Scripts/Player/GamePlayer.cs
+++ Assets/Scripts/Player/GamePlayer.cs
@@ -10,6 +10,7 @@ public class GamePlayer : NetworkBehaviour
     [SerializeField] GameObject PlayerHead;
 
     Vector3 _moveDir;
+    Vector2 dir;
 
     [SyncVar, SerializeField] private float _moveSpeed;
     [SerializeField] private float _rotateSpeed;
@@ -27,18 +28,20 @@ public class GamePlayer : NetworkBehaviour
 
     void MovePlayer()
     {
+        _moveDir = PlayerHead.transform.TransformDirection(new Vector3(dir.x, 0, dir.y)) * _moveSpeed * Time.deltaTime;
         _controller.SimpleMove(_moveDir);
     }
 
     public void OnMove(InputValue val)
     {
-        Vector2 dir = val.Get<Vector2>();
-        _moveDir = new Vector3(dir.x, 0, dir.y) * _moveSpeed * Time.deltaTime;
+        dir = val.Get<Vector2>();
     }
 
     public void OnRotate(InputValue val)
     {
         Vector2 delta = val.Get<Vector2>();
-        PlayerHead.transform.Rotate(delta * _rotateSpeed * Time.deltaTime);
+        Vector2 rotateVector = new Vector2(-delta.y, delta.x);
+        PlayerHead.transform.Rotate(rotateVector * _rotateSpeed * Time.deltaTime);
+        PlayerHead.transform.eulerAngles = new Vector3(PlayerHead.transform.eulerAngles.x, PlayerHead.transform.eulerAngles.y, 0);
     }
 }
---
+++ Assets/Scripts/UI/RoomUI.cs
@@ -0,0 +1,23 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class RoomUI : NetworkBehaviour
+{
+    [SerializeField] TMP_Text Text_People;
+    StringBuilder _peoples = new();
+
+
+    private void Update()
+    {
+        ShowConnectedPlayers();
+    }
+
+    void ShowConnectedPlayers()
+    {
+        Text_People.text = $"{NetworkManager.singleton.numPlayers} / {NetworkManager.singleton.maxConnections}";
+    }
+}
